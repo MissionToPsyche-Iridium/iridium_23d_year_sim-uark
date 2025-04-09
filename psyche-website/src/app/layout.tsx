c4145061c6aeda54a2c9729a5cf5f10af2fb4f5e
Vishal Jeyam <vishaljeyam@icloud.com>
2025-04-09 13:12:07 -0500

    corrected import case for layout.css

--- psyche-website/src/app/layout.tsx
+++ psyche-website/src/app/layout.tsx
@@ -3,7 +3,7 @@ export const metadata = {
   description: "An interactive website to deliver information to users through text and clickable elements",
 };
 
-import "../styles/layout.css";
+import "../styles/Layout.css";
 import InteractiveSection from "../components/InteractiveSection";
 import SplitSection from "../components/SplitSection";
 import FeatureSection from "../components/FeatureSection";
