6d1d572615a2cd584247432399c8a395b3d10264
Vishal Jeyam <vishaljeyam@icloud.com>
2025-04-18 11:30:28 -0500

    remove MarsScene component from LayoutContent

--- psyche-website/src/components/LayoutContent.tsx
+++ psyche-website/src/components/LayoutContent.tsx
@@ -82,7 +82,7 @@ export default function LayoutContent({ children }: { children: React.ReactNode 
       {/* <SplitSection />
       <SplitSectionTwo /> */}
       <CustomCursor />
-      <MarsScene />
+      {/* <MarsScene /> */}
       {/* <LaunchScene /> */}
     </>
   );
