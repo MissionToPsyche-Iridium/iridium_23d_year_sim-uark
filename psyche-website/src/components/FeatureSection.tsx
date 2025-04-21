a6df0ef6dab79a1d0572842bb2795b4d33ecd93a
Cassie Smith <cassiesmith3418@gmail.com>
2025-04-21 16:14:14 -0500

    added development team section

--- psyche-website/src/components/FeatureSection.tsx
+++ psyche-website/src/components/FeatureSection.tsx
@@ -1,4 +1,3 @@
-
 import "../styles/FeatureSection.css";
 
 export default function FeatureSection() {
@@ -6,51 +5,64 @@ export default function FeatureSection() {
     <section className="feature-section">
       <div>
         <h1 className="header-title">Citations</h1>
-        <a
-          href="https://psyche.asu.edu/mission/the-asteroid/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button-link"
-        >
-          The Asteroid
-        </a>
-        <a
-          href="https://psyche.asu.edu/mission/the-spacecraft/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button-link"
-        >
-          The Spacecraft
-        </a>
-        <a
-          href="https://psyche.asu.edu/mission/launch/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button-link"
-        >
-          The Launch
-        </a>
-        <a
-          href="https://psyche.asu.edu/mission/faq/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button-link"
-        >
-          Psyche FAQ
-        </a>
-        <a
-          href="https://www.jpl.nasa.gov/press-kits/psyche/quick-facts/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button-link"
-        >
-          Quick Facts
-        </a>
+        <div className="button-container">
+          <a
+            href="https://psyche.asu.edu/mission/the-asteroid/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button-link"
+          >
+            The Asteroid
+          </a>
+          <a
+            href="https://psyche.asu.edu/mission/the-spacecraft/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button-link"
+          >
+            The Spacecraft
+          </a>
+          <a
+            href="https://psyche.asu.edu/mission/launch/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button-link"
+          >
+            The Launch
+          </a>
+          <a
+            href="https://psyche.asu.edu/mission/faq/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button-link"
+          >
+            Psyche FAQ
+          </a>
+          <a
+            href="https://www.jpl.nasa.gov/press-kits/psyche/quick-facts/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button-link"
+          >
+            Quick Facts
+          </a>
+        </div>
+        <div>
+        <h1 className="header-title">Development Team</h1>
+        <div className="button-container">
+          <a className="credit-item">Vishal Jeyam</a>
+          <a className="credit-item">Amber Morton</a>
+          <a className="credit-item">Magaret Pedro Milne</a>
+          <a className="credit-item">Jacob Round</a>
+          <a className="credit-item">Cassie Smith</a>
+          <a className="credit-item">Lei Taylor</a>
+        </div>
+        </div>
       </div>
       {/* Disclaimer Section */}
       <div className="disclaimer-section">
         <p>
-          This work was created in partial fulfillment of The University of Arkansas Course “CSCE 49603″. The work is a result of the Psyche Student Collaborations component of NASA’s Psyche Mission (https://psyche.asu.edu). “Psyche: A Journey to a Metal World” [Contract number NNM16AA09C] is part of the NASA Discovery Program mission to solar system targets. Trade names and trademarks of ASU and NASA are used in this work for identification only. Their usage does not constitute an official endorsement, either expressed or implied, by Arizona State University or National Aeronautics and Space Administration. The content is solely the responsibility of the authors and does not necessarily represent the official views of ASU or NASA.
+          This work was created in partial fulfillment of The University of Arkansas Course “CSCE 49603″. The work is a result of the Psyche Student Collaborations component of NASA’s Psyche Mission (https://psyche.asu.edu). “Psyche: A Journey to a Metal World” [Contract number NNM16AA09C] is part of the NASA Discovery Program mission to solar system targets. Trade names and trademarks of ASU and NASA are used in this work for identification only. Their usage does not constitute an official endorsement, either expressed or implied, by Arizona State University or National Aeronautics and Space Administration. The content is solely the responsibility of the authors and does not necessarily represent the official views of ASU or NASA.
         </p>
       </div>
     </section>
