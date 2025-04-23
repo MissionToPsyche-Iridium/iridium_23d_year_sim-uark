b3cbf338eeb466f4e06871744d57bb114d3a025e
LeiTay <157653115+LeiTay@users.noreply.github.com>
2025-04-22 21:32:09 -0500

    Update page.tsx widget buttons
    widget button title fix to be more descriptive/not duplicated

--- psyche-website/src/app/home/page.tsx
+++ psyche-website/src/app/home/page.tsx
@@ -7,18 +7,18 @@ export default function Home() {
   return (
     <section className="feature-section">
       <div className="widget welcome-widget">
-        <h2 className="widget-title">🌌 Welcome to Year of Psyche</h2>
+        <h2 className="widget-title">🌌 Click to view a model comparing the sizes of the Earth, Mars, Moon, and Ceres to Psyche.</h2>
         <Link href="/compare">
           <button className="launch-button">
-            🚀 Launch Comparison
+            🌔 Planetary Size Comparison
           </button>
         </Link>
       </div>
       <div className="widget welcome-widget">
-        <h2 className="widget-title">🌌 Welcome to Year of Psyche</h2>
+        <h2 className="widget-title">🌌 Click to view an animation showcasing the Psyche spacecraft from liftoff to its destination.</h2>
         <Link href="/animation">
           <button className="launch-button">
-            Flight Path Animation
+            🚀 Flight Path Animation
           </button>
         </Link>
       </div>
