f1fa344893fe6e3f0058fdb6c621d4abe81811e6
NASAPsyche <psychemission@gmail.com>
2025-05-14 09:53:32 -0700

    added next config

--- psyche-website/next.config.ts
+++ psyche-website/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/iridium_23d_year_sim-uark',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
